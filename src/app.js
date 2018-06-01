c745fa9728b31e0fcf63515ade407cc90bad896a
temool <975728011@qq.com>
2018-06-01 20:46:01 +0800

    demo

--- src/app.js
+++ src/app.js
@@ -1,5 +1,51 @@
-console.log('这是应用的开始')
+console.log('app start')
 import mutltlineEllipis from './index'
 
 const box = document.getElementById('box')
-mutltlineEllipis({box,inserString: '...全文'})
+const btn = document.getElementById('btn')
+const removeBtn = document.getElementById('removeBtn')
+let str = box.innerHTML
+let addStr = '<a href="#">我是添加的文字</a>'
+
+mutltlineEllipis({
+  container: box,
+  str,
+  insertHTML: {
+    tag: 'p',
+    text: '...查看详情',
+    className: 'btn'
+  },
+  show: true,
+  hide: true,
+  callback: function (isCutted) {
+    console.log('是否省略：' + isCutted)
+  }
+})
+
+btn.onclick = function (ev) {
+  const dateType = btn.getAttribute('data-type')
+
+  if (dateType === 'add')  {
+    str = addStr + str
+    btn.setAttribute('data-type', 'remove')
+    btn.innerHTML = '删除文字'
+  } else {
+    str = str.slice(addStr.length)
+    btn.setAttribute('data-type', 'add')
+    btn.innerHTML = '添加文字'
+  }
+  mutltlineEllipis({
+    container: box,
+    str,
+    insertHTML: {
+      tag: 'p',
+      text: '...查看详情',
+      className: 'btn'
+    },
+    show: true,
+    hide: true,
+    callback: function (isCutted) {
+      console.log('是否省略：' + isCutted)
+    }
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-console.log('这是插件的js')
 /**
+ * 单行、多行文本溢出，后面显示省略号或特殊文本
  * 
- * 
- * @export
- * @param {any} box 
+ * @param {any} { container, maxLine = 3, insertHTML = {tag: 'a', text: '', className: ''}, show = false, hide = false, callback } 
  */
-function mutltlineEllipis ({ box, maxLine = 1, inserString = '' }) {
-  var height = box.offsetHeight
-  var lineHeight = parseFloat(getComputedStyle(box)['line-height'])
+function mutltlineEllipis ({ container, str, maxLine = 1, insertHTML = {tag: 'a', text: '', className: ''}, show = false, hide = false, callback }) {
+  container.innerHTML = `<span class="origin-text">${str}</span>`
+  var offsetHeight = container.offsetHeight
+  var paddingTop = parseFloat(getComputedStyle(container)['padding-top'])
+  var paddingBottom = parseFloat(getComputedStyle(container)['padding-bottom'])
+  var borderTop = parseFloat(getComputedStyle(container)['border-top-width'])
+  var borderBottom = parseFloat(getComputedStyle(container)['border-bottom-width'])
+
+  var lineHeight = parseFloat(getComputedStyle(container)['line-height'])
   var isOverflow = false
-  // 只有行数 >= 2 时，才需要处理
+  var isCutted = false
+  var height = offsetHeight - paddingTop - paddingBottom + borderTop + borderBottom
+  // 判断高度是否超出
   if (height > maxLine * lineHeight) {
     isOverflow = true
   } else {
@@ -17,9 +23,44 @@ function mutltlineEllipis ({ box, maxLine = 1, inserString = '' }) {
   }
   console.log('需要处理吗：' + isOverflow)
 
-  var cut = function cut () {
-    if (box.offsetHeight > maxLine * lineHeight) {
-      const originText = box.querySelector('.origin-text')
+  if (isOverflow) {
+    container.innerHTML = `<span class="origin-text">${str}</span>`
+    const originText = container.innerHTML
+    container.innerHTML += `<${insertHTML.tag} href="#" class="insert-node ${insertHTML.className}">${insertHTML.text}</${insertHTML.tag}>`
+    const insertNode = container.querySelector('.insert-node')
+    insertNode.style.display = 'inline'
+    insertNode.style.padding = 0
+    cut()
+    // 展开、关闭
+    container.onclick = function (e) {
+      const insertNode = container.querySelector('.insert-node')
+      const originTextNode = container.querySelector('.origin-text')
+      if (e.target === insertNode) {
+        if (show) {
+          if (isCutted) {
+            if (hide) {
+              insertNode.innerHTML = '收起'
+            } else {
+              insertNode.remove()
+            }
+            originTextNode.innerHTML = originText
+            isCutted = false
+
+            callback(false)
+          } else {
+            insertNode.innerHTML = insertHTML.text
+            cut()
+
+            callback(true)
+          }
+        }
+      }
+    }
+  }
+  // 裁剪文字
+  function cut () {
+    while (container.offsetHeight - paddingTop - paddingBottom - borderTop - borderBottom > maxLine * lineHeight) {
+      const originText = container.querySelector('.origin-text')
       const last = findLastNode(originText)
       let str = last.innerHTML || last.nodeValue
       if (!str) {
@@ -31,21 +72,10 @@ function mutltlineEllipis ({ box, maxLine = 1, inserString = '' }) {
           last.innerHTML = str.slice(0, str.length - 1)
         }
       }
-      cut()
-    } else {
-      console.log('处理完毕')
     }
+    console.log('处理完毕')
+    isCutted = true
   }
-  if (isOverflow) {
-    box.innerHTML = `<span class="origin-text">${box.innerHTML}</span>`
-    box.innerHTML += `<span class="insert-node">${inserString}</span>`
-    if (box.offsetHeight >= maxLine * lineHeight) {
-      console.log(box.querySelector('.origin-text').childNodes)
-      console.log(findLastNode(box.querySelector('.origin-text')))
-      cut()
-    }
-  }
-  console.log('需要处理吗：' + isOverflow)
   // 找到最后一个 node
   function findLastNode (node) {
     let childNodes = node.childNodes
